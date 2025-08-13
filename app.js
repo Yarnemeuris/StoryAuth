96e1b3bc8fd1924b7ad30c6129e7f82c42dce2cd
Yarnemeuris <yarne.meuris@gmail.com>
2025-08-13 11:08:09 +0200

    added saving and loading of user data

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const fs = require('node:fs');
+const readline = require("readline")
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const port = 8080;
 
-app.use(express.static('public', {"maxAge": 1000}))
+var users = {};
 
-app.listen(port)
+app.use(express.static('public', { "maxAge": 1000 }));
+
+app.get("/signup/usernameInUse/:name", (req, res) => {
+    res.json([users.keys().indexOf(req.params.name) != -1]);
+});
+
+rl.on("line", (input) => {
+    if (input != "exit" && input != "save") return
+
+    fs.writeFileSync('./data.json', JSON.stringify(users), 'utf8');
+    if (input == "save") return
+
+    server.close();
+    process.exit(0);
+});
+
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log(data);
+    users = JSON.parse(data);
+});
+
+var server = app.listen(port);
