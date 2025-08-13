ab48bf626aa3daae38cb67efcb7a579f082c74fd
Yarnemeuris <yarne.meuris@gmail.com>
2025-08-13 22:06:28 +0200

    added account creation

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
-const fs = require('node:fs');
+const fs = require('fs');
 const readline = require("readline")
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const port = 8080;
 
 var users = {};
 
 app.use(express.static('public', { "maxAge": 1000 }));
+app.use(express.json());
+
+function usernameInUse(name) {
+    return Object.keys(users).indexOf(name) != -1
+}
 
 app.get("/signup/usernameInUse/:name", (req, res) => {
-    res.json([users.keys().indexOf(req.params.name) != -1]);
+    res.json([usernameInUse(req.params.name)]);
+});
+
+app.post("/signup", async (req, res) => {
+    if (usernameInUse(req.body.name)) {
+        res.status(409).end();
+        return;
+    }
+
+    const hash = await bcrypt.hash(req.body.story, saltRounds);
+    users[req.body.username] = { hash: hash };
+    res.status(201).end();
 });
 
 rl.on("line", (input) => {
--- public/main.js
+++ public/main.js
@@ -2,3 +2,23 @@ import * as utils from "./js/utils.js";
 import "./js/edit.js";
 
 utils.switchToView("signUp")
+
+utils.addEventListenerFromID("usernameInput", "input", async (event) => {
+    const value = event.target.value
+    if (value == "") return
+
+    const response = await fetch("/signup/usernameInUse/" + value, {}).then((res) => res.json());
+
+    if (event.target.value != value) return
+    const available = !response[0]
+    document.getElementById("usernameAvailable").style.display = available ? "none" : "initial";
+});
+
+utils.addEventListenerFromID("createAccountButton", "click", async () => {
+    const username = document.getElementById("usernameInput").value
+    const story = document.getElementById("story").innerHTML.replaceAll(" ", "").replaceAll("\n", "")
+
+    if (document.getElementById("usernameAvailable").style.display != "none") return
+
+    await fetch("/signup", { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ username: username, story: story }) }).then(res => { console.log("Request complete! response:", res); });
+});
